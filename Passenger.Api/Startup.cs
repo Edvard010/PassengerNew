c56801146fa332db287f27732fb362c889f675db
Edvard010 <piszczatowski.grzegorz@gmail.com>
2020-03-21 15:52:55 +0100

    added classes to Domain

--- Passenger.Api/Startup.cs
+++ Passenger.Api/Startup.cs
@@ -26,6 +26,7 @@ namespace Passenger.Api
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+            //services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
